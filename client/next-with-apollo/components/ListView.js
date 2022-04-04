8c27c148af184bfdc06ba1307b32fa1283971f23
BhattiBytes <bhattibytes@gmail.com>
2022-04-04 16:45:01 -0400

    Update - Pagination page numbers & make responsive

--- client/next-with-apollo/components/ListView.js
+++ client/next-with-apollo/components/ListView.js
@@ -8,22 +8,36 @@ const ListView = ({ colors, handleClickedSwatch }) => {
   const slicedColors = colors?.slice(startIndex, endIndex);
 
   return (
-    <div className={styles.listViewContainer}>
-     { slicedColors?.map(swatch => {
-        return (
-          <div key={swatch.id} className={styles.listViewSwatches}>
-            <img role="presentation" className={styles.swatchImg} width={200} height={200} style={{backgroundColor: swatch.hsl}}
-            onClick={() => handleClickedSwatch(swatch)}/>
-            <h2 className={styles.swatchDets}>HEX {swatch.hex}</h2>
-            <h2 className={styles.swatchDets}>{swatch.rgb}</h2>
-          </div>
-        )
-     })}
-      <div className={styles.paginationContainer}>
-        <button className={styles.paginationButton} onClick={() => setPageNumber(pageNumber - 1)}>Previous</button>
-        <button className={styles.paginationButton} onClick={() => setPageNumber(pageNumber + 1)}>Next</button>
+    <div>
+      <div className={styles.listViewContainer}>
+      { slicedColors?.map(swatch => {
+          return (
+            <div key={swatch.id} className={styles.listViewSwatches}>
+              <img role="presentation" className={styles.swatchImg} width={200} height={200} style={{backgroundColor: swatch.hsl}}
+              onClick={() => handleClickedSwatch(swatch)}/>
+              <h2 className={styles.swatchDets}>HEX {swatch.hex}</h2>
+              <h2 className={styles.swatchDets}>{swatch.rgb}</h2>
+            </div>
+          )
+      })}
+        
       </div>
+    <div className={styles.paginationContainer}>
+      <h3 onClick={() => setPageNumber(1)}>1</h3>
+      <h3 onClick={() => setPageNumber(2)}>2</h3>
+      <h3 onClick={() => setPageNumber(3)}>3</h3>
+      <h3 onClick={() => setPageNumber(4)}>4</h3>
+      <h3 onClick={() => setPageNumber(3)}>3</h3>
+      <h3 onClick={() => setPageNumber(6)}>6</h3>
+      <h3 onClick={() => setPageNumber(7)}>7</h3>
+      <h3 onClick={() => setPageNumber(8)}>8</h3>
+      <h3 onClick={() => setPageNumber(9)}>9</h3>
+      <h3 onClick={() => setPageNumber(10)}>10</h3>
+      <h3 onClick={() => setPageNumber(11)}>11</h3>
+      <h3 onClick={() => setPageNumber(12)}>12</h3>
+      <h3 onClick={() => setPageNumber(13)}>13</h3>
     </div>
+  </div>
   )
 };
 
--- server/API/server.js
+++ server/API/server.js
@@ -8,24 +8,6 @@ const PORT = 8080;
 
 app.use(cors())
 
-// app.use((req, res, next) => {
-//   res.setHeader('Access-Control-Allow-Origin', '*');
-//   res.setHeader(
-//     'Access-Control-Allow-Methods',
-//     'GET'
-//   );
-//   res.setHeader(
-//     'Access-Control-Allow-Headers',
-//     'Content-Type'
-//   );
-
-//   if (req.method === 'OPTIONS') {
-//     return res.sendStatus(204);
-//   } else {
-//     next();
-//   }
-// });
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
