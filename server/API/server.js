57f62343f60cc46626d7ea20d2e236fa86f1866a
BhattiBytes <bhattibytes@gmail.com>
2022-04-04 14:58:05 -0400

    Update Server for CORS error

--- server/API/server.js
+++ server/API/server.js
@@ -6,7 +6,26 @@ const app = express();
 
 const PORT = 8080;
 
-app.use(cors())
+// app.use(cors())
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PUT, DELETE, PATCH'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type'
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(204);
+  } else {
+    next();
+  }
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
